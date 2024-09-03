9c783e972260bb810a68d01ba6ad93f15c44b4bd
Ron <nazarmuhamedow9731@gmail.com>
2024-09-04 00:26:47 +0500

    Created rootStyles

---
+++ src/constants/rootStyles.ts
@@ -0,0 +1,15 @@
+export const theme = {
+    dark: {
+        background: "#111111",
+        primaryColor: "#0F63FF",
+        secondary: "#282828",
+        textColor: "#fff",
+
+    },
+    light: {
+        background: "#111111",
+        primaryColor: "",
+        secondary: "",
+        textColor: "#fff",
+    }
+}
