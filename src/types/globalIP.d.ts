fb61f1870676c03b24fa27d2b3cb2076f8d89bc5
Ron <nazarmuhamedow9731@gmail.com>
2024-10-06 22:26:56 +0500

    Created interface IGlobalIP

---
+++ src/types/globalIP.d.ts
@@ -0,0 +1,26 @@
+export interface IGlobalIP {
+    as: string;
+    asname: string;
+    city: string;
+    continent: string;
+    continentCode: string;
+    country: string;
+    countryCode: string;
+    currency: string;
+    district: string;
+    hosting: boolean;
+    isp: string;
+    lat: number;
+    lon: number;
+    mobile: boolean;
+    offset: number;
+    org: string;
+    proxy: boolean;
+    query: string;
+    region: string;
+    regionName: string;
+    reverse: string;
+    status: string;
+    timezone: string;
+    zip: string;
+}
