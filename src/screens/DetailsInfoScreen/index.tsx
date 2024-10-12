9c40955df1529da0f4b74e7435afed5d913b1a87
Ron <nazarmuhamedow9731@gmail.com>
2024-10-12 23:39:34 +0500

    Added the useRoute hook to access route parameters, and the component's props type was updated

--- src/screens/DetailsInfoScreen/index.tsx
+++ src/screens/DetailsInfoScreen/index.tsx
@@ -1,12 +1,14 @@
 import { RouteProp, useRoute } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { View } from "react-native";
+import { IGlobalConnection, ILocalConnection } from "../../types/connectionTypes";
 
 type Props = NativeStackScreenProps<HomeStack, "DetailsInfoScreen">
 type DetailsInfoScreenRouteProp = RouteProp<HomeStack, "DetailsInfoScreen">
 
 export default function DetailsInfoScreen({ navigation }: Props) {
     const router = useRoute<DetailsInfoScreenRouteProp>();
+    const { isLocal, ipData } = router.params as ILocalConnection | IGlobalConnection
    
 
     return (
