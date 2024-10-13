6203735e8b4a37d807322781650aadb73510dfa2
Ron <nazarmuhamedow9731@gmail.com>
2024-10-13 18:51:07 +0500

    Changed type & added default value for parameter 'value' in ListItem component

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -4,13 +4,14 @@ import { IListItem } from "./ListItem.props";
 
 
 export default function ListItem({ name, value }: IListItem) {
-     return (
+    const emDash = "\u2014" 
+    return (
         <View style={localInfoStyles.row}>
             <Text style={[localInfoStyles.text, localInfoStyles.name]}>
                 {name}:
             </Text>
             <Text style={[localInfoStyles.text, localInfoStyles.value]}>
-                {value}
+                {value || emDash}
             </Text>
         </View>
     )   
