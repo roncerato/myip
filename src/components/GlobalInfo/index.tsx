c92d9e2f1d960aface0a5776f075b737827ad5fe
Ron <nazarmuhamedow9731@gmail.com>
2024-10-13 19:03:03 +0500

    Added API data and flag display to GlobalInfo, replaced static values with dynamic ones

--- src/components/GlobalInfo/index.tsx
+++ src/components/GlobalInfo/index.tsx
@@ -1,37 +1,49 @@
 import React from "react";
-import { ScrollView, View, Text } from "react-native";
+import { ScrollView, View, Text, Image } from "react-native";
 import ListItem from "../ListItem";
 import { globalInfoStyles } from "./GlobalInfo.style";
 import MainBlock from "../MainBlock";
 import { IGlobalInfo } from "./GlobalInfo.props";
+import { theme } from "../../constants/rootStyles";
+import { ipInfoStyles } from "../IPInfo/IPInfo.style";
 
 
-export default function GlobalInfo({ data }: IGlobalInfo) {
+export default function GlobalInfo({ data, flag }: IGlobalInfo) {
     return (
         <ScrollView>
             <View style={globalInfoStyles.container}>
                 <View style={globalInfoStyles.country}>
-                    <View style={globalInfoStyles.countryImg}>
+                    <View
+                        style={[
 
+                            ipInfoStyles.flag,
+                            { backgroundColor: theme.dark.primaryColor },
+                            ipInfoStyles.image
+                        ]}
+                    >
+                        {
+                            flag !== undefined ? <Image source={flag} style={{ width: "100%", height: "100%", resizeMode: "cover" }}
+                            /> : null
+                        }
                     </View>
 
                     <Text style={globalInfoStyles.countryName}>
-                        Германия
+                        {data?.country}
                     </Text>
 
                 </View>
-                    <MainBlock name={"IP"} value={"142.111.162.207"} />
+                <MainBlock name={"IP"} value={data?.query} />
 
                 <View style={globalInfoStyles.mainBlock}>
-                    <MainBlock name={"Широта"} value={"52.5229"} />
-                    <MainBlock name={"Долгота"} value={"13.4896"} />
+                    <MainBlock name={"Широта"} value={String(data?.lat)} />
+                    <MainBlock name={"Долгота"} value={String(data?.lon)} />
                 </View>
 
-                <ListItem name={"Проdайдер"} value={"InterLIR GmbH"} />
-                <ListItem name={"Прокси"} value={"Не используется"} />
-                <ListItem name={"Город"} value={"Берлин"} />
-                <ListItem name={"Континент"} value={"Европа"} />
-                <ListItem name={"Код страны"} value={"DE"} />
+                <ListItem name={"Провайдер"} value={data?.asname} />
+                <ListItem name={"Прокси"} value={data?.proxy ? "Используется" : "Не используется"} />
+                <ListItem name={"Город"} value={data?.city} />
+                <ListItem name={"Континент"} value={data?.continent} />
+                <ListItem name={"Код страны"} value={data?.countryCode} />
             </View>
         </ScrollView>
     )
