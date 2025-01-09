cd1333a2dbf2fbc731696939a181cd14da2fbde5
ruslannazari <nazarmuhamedowruslan@gmail.com>
2025-01-09 15:53:18 +0500

    The styles of the LangDropdown component have been updated: new properties for borders and backgrounds have been added, and the structure of displaying list items has been improved.

--- src/components/LangDropdown/index.tsx
+++ src/components/LangDropdown/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { FlatList, Image, StyleSheet, Text, View } from "react-native";
-import { theme } from "../../constants/rootStyles";
 import ChevronDown from "../../../assets/images/svg/down-chevron.svg";
 import { useTranslation } from "react-i18next";
 import { langDropdownStyles } from "./LangDropdown.style";
@@ -31,30 +30,41 @@ export default function LangDropdown() {
 
     return (
         <View style={langDropdownStyles.container}>
-            <View style={langDropdownStyles.dropdownSelected} onTouchEnd={() => setIsOpened(!isOpened)}>
+            <View style={[langDropdownStyles.dropdownSelected, { borderColor: colors.border }]} onTouchEnd={() => setIsOpened(!isOpened)}>
                 <Text style={[langDropdownStyles.dropdownSelectedText, { color: colors.text }]}>{lang}</Text>
-                <ChevronDown width={10} height={10} fill={theme.dark.lightgrey} />
+                <ChevronDown width={10} height={10} fill={colors.secondText} />
             </View>
             {
                 isOpened &&
-                <FlatList
-                    contentContainerStyle={langDropdownStyles.dropdownContainer}
-                    // style={langDropdownStyles.dropdownContainer}
-                    data={langs}
-                    renderItem={({ item }) => (
-                        <View style={{ display: "flex", flexDirection: "row", gap: 6 }}>
-                            <View style={langDropdownStyles.dropdownItemIcon}>
-                                <Image
-                                    source={item.icon}
-                                    style={{ width: "100%", height: "100%", resizeMode: "cover" }} />
+                <View
+                    style={[langDropdownStyles.dropdownContainer, {
+                        borderColor: colors.border,
+                        backgroundColor: colors.card,
+                    }]}
+                >
+                    <FlatList
+                        data={langs}
+                        keyExtractor={item => item.key}
+                        contentContainerStyle={{
+                            rowGap: 8
+                        }}
+                        renderItem={({ item }) => (
+                            <View style={langDropdownStyles.dropdownItem}>
+                                <View
+                                    style={langDropdownStyles.dropdownItemIcon}>
+                                    <Image
+                                        source={item.icon}
+                                        style={{ width: "100%", height: "100%", resizeMode: "cover" }} />
+                                </View>
+                                <Text
+                                    style={[langDropdownStyles.dropdownItemText, { color: colors.secondText }]}
+                                    onPress={() => changeLanguage(item.key, item.label)}>
+                                    {item.label}
+                                </Text>
                             </View>
-                            <Text style={[langDropdownStyles.dropdownItemText, { color: colors.secondText }]} onPress={() => changeLanguage(item.key, item.label)}>
-                                {item.label}
-                            </Text>
-                        </View>
-                    )}
-                    keyExtractor={item => item.key}
-                />
+                        )}
+                    />
+                </View>
             }
         </View>
     );
